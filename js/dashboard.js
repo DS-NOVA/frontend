e45ea0576f9032f21b101ccb2b97edafe1606fba
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-06-01 14:23:17 +0900

    [feat] 영상 overlay 기능 #9

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const fileInput = document.getElementById('videoInput');
   const inputVideo = document.getElementById('inputVideo');
   const outputVideo = document.getElementById('outputVideo');
+  const overlayVideo = document.getElementById('overlayVideo');
 
+  const startTime = 3, endTime = 5; //추후에 시작, 끝 타임 가져오기
 
   uploadButton.addEventListener('click', () => {
     fileInput.click();
@@ -11,11 +13,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
   fileInput.addEventListener('change', () => {
   const file = fileInput.files[0];
+
   if (file) {
     const videoURL = URL.createObjectURL(file);
     inputVideo.src = videoURL;
     inputVideo.load();
     inputVideo.play();
+
+    /*추후 변경*/
+    outputVideo.src = videoURL;
+    outputVideo.load();
+    overlayVideo.load();
+
+    outputVideo.play();
+
+    //결과 비디오 시작 시
+    outputVideo.addEventListener("timeupdate", () => {
+      const currentTime = outputVideo.currentTime; 
+        if(currentTime >= startTime && currentTime <= endTime){
+          console.log("it's overlay");
+          if(overlayVideo.paused && overlayVideo.style.display ==="none"){
+            overlayVideo.style.display = "block";
+            overlayVideo.play();
+          }
+      }else{//지정 시간 외에
+        if (!overlayVideo.paused && overlayVideo.style.display === "block"){
+          overlayVideo.pause();
+          overlayVideo.style.display = "none";
+        } 
+      }
+    });
+    outputVideo.addEventListener('pause', () => {
+    overlayVideo.pause();
+  });
+    
   }
 });
 });
