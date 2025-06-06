5fd0e948c02ec7ed36a124182e93f0adcfa86bb8
Yujung Kim <kimyj4017@gmail.com>
2025-06-06 21:20:00 +0900

    [feat] 선택한 영상 api 요청 추가 #10

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const fileInput = document.getElementById('videoInput');
   const inputVideo = document.getElementById('inputVideo');
   const outputVideo = document.getElementById('outputVideo');
-
+  let selectedFile = null;
 
   uploadButton.addEventListener('click', () => {
     fileInput.click();
@@ -18,4 +18,31 @@ document.addEventListener('DOMContentLoaded', () => {
     inputVideo.play();
   }
 });
+});
+
+document.getElementById('dashboard-play').addEventListener('click', async () => {
+  const file = document.getElementById('videoInput').files[0];
+  if (!file) {
+    alert('파일을 선택해주세요');
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append('video', file);
+
+  try {
+    const response = await fetch('http://127.0.0.1:8000/nova/dashboard/video/upload/', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const result = await response.json();
+    
+    console.log(result);
+    alert(result.message);
+  } catch (err) {
+    console.error(err);
+    // 왜 자꾸 서버에 잘 올라가는데 업로드 실패가 뜨는지..? 모르겠음 추후 수정 예정
+    // alert('업로드 실패'); 
+  }
 });
